322b18f86b89a87c4b72cb1773d471c83f1a10f5
Felipe-Santana <felipesantanaalbuquerque9@gmail.com>
2020-10-17 14:56:47 -0300

    :sparkles: Add documents to deliveryman model

--- src/model/Deliveryman.ts
+++ src/model/Deliveryman.ts
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 import { nanoid } from "nanoid";
-import documentSchema from "./DocumentSchema";
+import { AddressSchema } from "./Address";
+import { CnhSchema } from "./Cnh";
 
 const Schema = mongoose.Schema;
 
-const DeliverymanSchema = new Schema(
+export const DeliverymanSchema = new Schema(
   {
     _id: {
       type: String,
@@ -25,11 +26,30 @@ const DeliverymanSchema = new Schema(
       type: String,
       required: true,
     },
-    document: documentSchema,
+    rg: {
+      number: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 9
+      }
+    },
+    cpf: {
+      number: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 11,
+        maxLength: 11
+      }
+    },
     phone_number: {
       type: String,
       trim: true,
+      required: true
     },
+    cnh: CnhSchema,
+    address: AddressSchema,
   },
   {
     timestamps: {
@@ -39,6 +59,4 @@ const DeliverymanSchema = new Schema(
   }
 );
 
-const Deliveryman = mongoose.model("Deliveryman", DeliverymanSchema);
-
-export default Deliveryman;
+export const Deliveryman = mongoose.model("Deliveryman", DeliverymanSchema);
