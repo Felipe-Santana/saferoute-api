557be7dbd9b147ba46f89c0a1913afdb5f5edf94
Felipe-Santana <felipesantanaalbuquerque9@gmail.com>
2020-10-17 14:57:50 -0300

    :sparkles: Import deliveryman routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import businessRouter from "./routes/businessRoutes";
+import deliverymanRoutes from "./routes/deliverymanRoutes";
 
 const router = Router();
 
 router.use("/business", businessRouter);
+router.use("/deliveryman", deliverymanRoutes);
 
 export default router;
